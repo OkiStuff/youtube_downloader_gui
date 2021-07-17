680bd8ff6d78a1dcf1a27b3eef0ea4a0ba1700c5
OkiStuff <probro1802@gmail.com>
2021-07-16 23:45:27 -0400

    rest of code

---
+++ Youtube Downloader/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Youtube_Downloader
+{
+    public partial class Form1 : Form
+    {
+
+        String url;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            url = textBox1.Text;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("youtubedl.exe ", url);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
